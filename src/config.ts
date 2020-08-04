868498926abfec15d9c950569e7304921d912519
ybguomoltin <56880725+ybguomoltin@users.noreply.github.com>
2020-08-04 13:44:50 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 
 export const config = {
   clientId: 'EdP3Gi1agyUF3yFS7Ngm8iyodLgbSR3wY4ceoJl0d2',
-  stripeKey: 'pk_test_JwRX4cevuCysIEQrpwcbn3j8',
+  stripeKey: 'pk_test_vJ03MLfK1iKnqUZJngyIcgs900FIrn4kOP',
   categoryPageSize: 8,
   maxCompareProducts: 4,
   algoliaAppId: 'L1TT97EBOE',
